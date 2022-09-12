16ce976eae1a183b8b30bc5117fa86c89a3f8aaa
Carles Oliveira <charles.oliveira@facens.br>
2022-09-12 20:31:39 -0300

    20220912

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,6 +12,29 @@ import { FerramentasDeDefesaComponent } from './components/ferramentas-de-defesa
 import { AdversarialMachineLearningComponent } from './components/adversarial-machine-learning/adversarial-machine-learning.component';
 import { DadosCardComponent } from './components/dados-recentes/dados-card/dados-card.component';
 import { FormularioComponent } from './components/formulario/formulario.component';
+
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "firebase/app";
+  import { getAnalytics } from "firebase/analytics";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+  
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCAt--EYykR6wFqllWyUm3Q0VVQbkL6aPA",
+    authDomain: "provaangular-c8575.firebaseapp.com",
+    projectId: "provaangular-c8575",
+    storageBucket: "provaangular-c8575.appspot.com",
+    messagingSenderId: "171037975622",
+    appId: "1:171037975622:web:767cb1971d8132d3779647",
+    measurementId: "G-EQ6LVXTCR6"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
 
 @NgModule({
   declarations: [
@@ -26,7 +50,8 @@ import { FormularioComponent } from './components/formulario/formulario.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-formulario',
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormularioComponent implements OnInit {
 
+  ngModelNome: string ='';
+
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
 
 }
