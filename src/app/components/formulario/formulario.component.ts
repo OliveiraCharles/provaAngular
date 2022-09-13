274a8475a41061d5ddcc334568c8f8ed31ca4978
Carles Oliveira <charles.oliveira@facens.br>
2022-09-13 08:37:00 -0300

    20220913

--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -7,13 +7,43 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class FormularioComponent implements OnInit {
 
-  ngModelNome: string ='';
+  ngModelNome: string = '';
 
+  nomeUsuario: string = '';
+  sobrenomeUsuario: string = '';
+  telefoneUsuario: string = '';
+  emailUsuario: string = '';
+  ruaUsuario: string = '';
+  cepUsuario: string = '';
+
+  numCasa: string = '';
+  stringFinalEmail = '';
+
+  destinoEmail: string = "mailto:charles.oliveira@facens.br;leonardo.souza@facens.br";
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  getVar(valor:any){
+    console.log(valor.nome);
+  }
+
+  variavel = this.getVar
+
+  concatEmail() {
+    
+    this.stringFinalEmail = `${this.destinoEmail} &subject=Mensagem de ${this.nomeUsuario} &body=Prezados%0D%0A Nome: ${this.nomeUsuario} ${this.sobrenomeUsuario}, %0D%0A 
+    Email: ${this.emailUsuario}, %0D%0A
+    telefone: ${this.telefoneUsuario}, %0D%0A
+    Endereço: ${this.cepUsuario}, %0D%0A %0D%0A
+    
+    Atenciosamente,%0D%0A
+    ${this.nomeUsuario} | ${this.telefoneUsuario}`
+
+    return this.stringFinalEmail;
+  };
 
 
 }
